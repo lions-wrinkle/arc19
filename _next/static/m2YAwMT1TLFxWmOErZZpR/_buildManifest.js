be2b6de0dc704194d6a42d9f36a02612244d295f
lions-wrinkle <94868564+lions-wrinkle@users.noreply.github.com>
2023-01-07 18:59:26 +0100

    Updates

--- _next/static/m2YAwMT1TLFxWmOErZZpR/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/212-08dffba4d66bc1a6.js","static/chunks/pages/index-bccc8c1e4d8cc80b.js"],"/_error":["static/chunks/pages/_error-15a0bbaa3c2350f1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
